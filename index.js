aa4e9562cb4370d6b967c048704d1b8c85f50b9b
GetMeOutOfBirmingham <116860644+GetMeOutOfBirmingham@users.noreply.github.com>
2022-10-27 21:58:34 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,14 @@
 document.addEventListener('DOMContentLoaded', Initialize);
 
 var Input1;
-var Input2;
 var EntBtn;
 
 function Initialize() {
     Input1 = document.getElementById('input1');
-    Input2 = document.getElementById('input2');
     EntBtn = document.getElementById('Enter');
     EntBtn.addEventListener('click', Print);
 }
 
 function Print() {
-    var InputVals = Input1.value + "+" + Input2.value;
-    alert(eval(InputVals));
+    alert(Input1);
 }
