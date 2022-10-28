99b36d6f2a2c71dfa06340c1340cd367216b168c
MyNameBruh <116854117+MyNameBruh@users.noreply.github.com>
2022-10-27 20:41:31 -0700

    index.js
    the main javascript file for this project

---
+++ index.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoader', Initialize);
+
+var Input1;
+var Input2;
+var EntBtn;
+
+function Initialize() {
+    Input1 = document.getElementById('Input1');
+    Input2 = document.getElementById('Input2');
+    EntBtn = document.getElementById('Enter');
+}
